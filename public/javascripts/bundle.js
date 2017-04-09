33bc6b76526c3c9d9f79bfbdaa0eb539ffa2463a
Nakshatra Mukhopadhyay <nakshatra.mukhopadhyay1994@gmail.com>
2017-04-09 20:04:21 +0530

    POSTed waybackified URL to express endpoint

--- public/javascripts/bundle.js
+++ public/javascripts/bundle.js
@@ -3,19 +3,8 @@ window.onload = function () {
   var inputTimeElement = document.getElementById('input-time');
   var waybackifyBtn = document.getElementById('waybackify-btn');
 
-  waybackifyBtn.addEventListener('click', (evt) => {
-    evt = evt || window.event;
-    evt && evt.preventDefault();
-    waybackifyUrl(inputUrlElement.value, inputTimeElement.value);
-  });
-
-  inputUrlElement.addEventListener('keydown', (evt) => {
-    evt = evt || window.event;
-    var keyCode = evt && (evt.keyCode || evt.which);
-    if (keyCode &&  keyCode === 13) {
-      waybackifyBtn && waybackifyBtn.click();
-    }
-  });
+  waybackifyBtn.addEventListener('click', handleWaybackifyClick);
+  inputUrlElement.addEventListener('keydown', handleReturnPress);
 
   function waybackifyUrl(url, timestamp) {
     if (url) {
@@ -23,6 +12,31 @@ window.onload = function () {
       return `https://web.archive.org/web/${timestamp}/${url}`;
     } else {
       return 'No url provided.';
+    }
+  }
+
+  function postUrl (url) {
+    var req = new XMLHttpRequest();
+    req.open('POST', '/', true);
+    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    req.send(`waybackifiedUrl=${encodeURIComponent(url)}`);
+  }
+
+  function handleWaybackifyClick(evt) {
+    var waybackifiedUrl = '';
+    evt = evt || window.event;
+    evt && evt.preventDefault();
+
+    waybackifiedUrl = waybackifyUrl(inputUrlElement.value, inputTimeElement.value);
+
+    postUrl(waybackifiedUrl);
+  }
+
+  function handleReturnPress (evt) {
+    evt = evt || window.event;
+    var keyCode = evt && (evt.keyCode || evt.which);
+    if (keyCode &&  keyCode === 13) {
+      waybackifyBtn && waybackifyBtn.click();
     }
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', { title: 'History Scraper' });
+});
+
+router.post('/', function(req, res) {
+  var waybackifiedUrl = req.body.waybackifiedUrl;
+  console.log(waybackifiedUrl);
 });
 
 module.exports = router;
